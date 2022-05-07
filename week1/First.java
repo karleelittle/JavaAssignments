a1767660ebab1ba63b7810ad0a2ff5581a6d6d96
Karlee Little <karleelittle1@gmail.com>
2022-05-07 00:02:19 -0700

    Publish to Github

---
+++ week1/First.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class First {
+        public static void main(String [] args) {
+            System.out.println("My name is Karlee Little");
+            System.out.println("I am 22 years old");
+            System.out.println("My hometown is Orange County, CA");
+        }
+    }
+
